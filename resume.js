62559f1b779b25c959700468184a26b28ce0a269
dastan888 <dastan.aibek8@gmail.com>
2020-07-11 16:49:16 +0200

    my resume

---
+++ resume.js
@@ -0,0 +1,46 @@
+let resume = {
+  Intro: [
+    {
+      Name: "Dastan Aibek",
+      Street: "Baarova",
+      City: "Prague",
+      Mail: "dastan.aibek8@gmail.com",
+      /* Phone: function(code = '+420', cell = '773192459') {
+                        return code + cell;
+                    }*/
+    },
+  ],
+  Certifications: [
+    "Admin ",
+    "AppBuilder ",
+    "PD1 ",
+    "Beer Expert ",
+    "Lovely Son",
+  ],
+  Experience: [
+    {
+      title: "Google CEO",
+      startDate: 2020,
+      endDate: 2016,
+      salary: "more than a lot",
+      duration: function () {
+        return this.endDate - this.startDate;
+      },
+    },
+    {
+      title: "Yahoo CEO",
+      startDate: 2010,
+      endDate: 2005,
+      salary: "a lot",
+      duration: function () {
+        return this.endDate - this.startDate;
+      },
+    },
+  ],
+  Education: ["BA in Yahoo", "MA in Google"],
+  Languages: ["English", "Czech", "Russian", "Kyrgyz", "Ukrainian"],
+  Hi: function () {
+    console.log('hi');
+  },
+};
+resume.Hi();
